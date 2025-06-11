8b505517c123a054d7c891727f42f503ac7e79ab
Flavien-Walk <flavien.walkowiak@epitech.digital>
2025-06-11 17:16:50 +0200

    retrait routes

--- server.js
+++ server.js
@@ -15,12 +15,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connecté à MongoDB"))
   .catch(err => console.error("❌ Erreur MongoDB :", err));
 
-// Routes
-const chatRoutes = require("./routes/chat");
-const userRoutes = require("./routes/user");
-
-app.use("/chat", chatRoutes);
-app.use("/user", userRoutes);
 
 // Lancer le serveur
 app.listen(PORT, () => {
